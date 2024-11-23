1d344de5331ec5cd412408c771b6fb4ac5e63000
Didik Ariyanto <didikariyanto301@gmail.com>
2024-11-23 15:47:06 +0700

    add models

---
+++ app/Models/Attendance.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $fillable = [
+        'sn',
+        'table',
+        'stamp',
+        'employee_id',
+        'timestamp',
+        'status1',
+        'status2',
+        'status3',
+        'status4',
+        'status5',
+        'created_at',
+        'updated_at'
+    ];
+
+    // Relasi dengan model User
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'employee_id', 'id'); // Menghubungkan employee_id dengan id di tabel users
+    }
+}
